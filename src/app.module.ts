c4333552aaf1e3f35c9d1adf7d38990cd996600f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-09 17:48:36 +0500

    connection PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      username: "postgres",
+      host: "localhost",
+      port: 5432,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
   ],
   controllers: [],
   providers: [],
