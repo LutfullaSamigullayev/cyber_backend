efff4b27414389d39e2f4d679fa74936da3ec246
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-10 20:35:00 +0500

    fix import paths for User entity in auth module and service

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "./entity/user.entity";
+import { User } from "./entities/user.entity";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { JwtStrategy } from "src/common/strategy/jwt.strategy";
