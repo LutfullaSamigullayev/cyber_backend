03d027c187227ff913deb62b27a1f3d13a75bb55
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-11 20:20:00 +0500

    added Swagger decorators for RegisterDto

--- src/module/auth/dto/register.dto.ts
+++ src/module/auth/dto/register.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class RegisterDto {
+  @ApiProperty({
+    example: "lutfulla@gmail.com",
+    description: "Foydalanuvchi emaili",
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: "123456",
+    minLength: 6,
+  })
   @IsString()
   @MinLength(6)
   password: string;
