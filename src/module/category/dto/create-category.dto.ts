8397f6fd6acd48e3a929d0b122dcf801f019c989
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-11 20:43:00 +0500

    added Swagger decorators for CreateCategoryDto

--- src/module/category/dto/create-category.dto.ts
+++ src/module/category/dto/create-category.dto.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, IsUrl, Length } from "class-validator";
 
 export class CreateCategoryDto {
+  @ApiProperty({
+    example: "Electronics",
+    minLength: 3,
+    maxLength: 50,
+    description: "Kategoriya nomi",
+  })
   @IsString()
   @Length(3, 50)
   name: string;
 
+  @ApiProperty({
+    example: "https://example.com/image.png",
+    description: "Kategoriya rasmi (URL)",
+  })
   @IsUrl()
   img: string;
 }
