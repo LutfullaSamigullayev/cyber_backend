1777613055c230248a01c160589fe816576a6203
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-11 20:32:00 +0500

    added Swagger decorators for AuthController

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -2,16 +2,20 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { RegisterDto } from "./dto/register.dto";
 import { LoginDto } from "./dto/login.dto";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Profile")
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: "Ro'yxatdan o'tish" })
   @Post("register")
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
 
+  @ApiOperation({ summary: "Login qilish" })
   @Post("login")
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
