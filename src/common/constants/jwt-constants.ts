80ca6c5535afcbeaa6875f8f2213603ac2e3952f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-11 21:14:00 +0500

    updated jwtConstants

--- src/common/constants/jwt-constants.ts
+++ src/common/constants/jwt-constants.ts
@@ -1,3 +1,4 @@
 export const jwtConstants = {
-  secret: String(process.env.JWT_SECRET_KEY as string)
+  secret: String(process.env.JWT_SECRET as string),
+  expiresIn: (process.env.JWT_EXPIRES || "7d") as any,
 };
