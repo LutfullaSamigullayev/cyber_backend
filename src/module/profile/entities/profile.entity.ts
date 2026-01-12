273e3f9292a34adc8d20e069ec42b627e63f0e78
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-12 15:54:00 +0500

    created profile entity

---
+++ src/module/profile/entities/profile.entity.ts
@@ -0,0 +1,30 @@
+import { User } from "src/module/auth/entities/user.entity";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from "typeorm";
+
+@Entity("profiles")
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true })
+  firstName: string;
+
+  @Column({ nullable: true })
+  lastName: string;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: true })
+  avatar: string;
+
+  @OneToOne(() => User, { onDelete: "CASCADE" })
+  @JoinColumn()
+  user: User;
+}
