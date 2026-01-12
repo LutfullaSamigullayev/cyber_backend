49903d8734d9aab3cf6e4a053a2d197b6148afd1
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-12 14:10:00 +0500

    created product entity

---
+++ src/module/product/entities/product.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("products")
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column("decimal", { precision: 10, scale: 2 })
+  price: number;
+
+  @Column({ default: 0 })
+  stock: number;
+
+  @Column({ type: "jsonb", nullable: true })
+  attributes: Record<string, any>;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
