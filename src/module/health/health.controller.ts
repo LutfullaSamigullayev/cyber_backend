3f2029543c04120a1f5701f67c38760f1d378f79
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-12 13:37:48 +0500

    update deploy.yaml

--- src/module/health/health.controller.ts
+++ src/module/health/health.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from "@nestjs/common";
+import { Public } from "src/common/decorators/public.decorator";
 
 @Controller("health")
 export class HealthController {
+  @Public()
   @Get()
   check() {
-    return {
-      status: "ok",
-      service: "cyber-backend",
-    };
+    return "Backend is healthy";
   }
 }
