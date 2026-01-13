0dcb2ff8fc5805de21f45a89c95d19c56bf46b27
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-13 22:23:00 +0500

    added swagger address dto

--- src/module/address/dto/create-address.dto.ts
+++ src/module/address/dto/create-address.dto.ts
@@ -1,22 +1,48 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { IsBoolean, IsOptional, IsString } from "class-validator";
 
 export class CreateAddressDto {
+  @ApiProperty({
+    example: "Uzbekistan",
+    description: "Davlat nomi",
+  })
   @IsString()
   country: string;
 
+  @ApiProperty({
+    example: "Tashkent",
+    description: "Shahar nomi",
+  })
   @IsString()
   city: string;
 
+  @ApiProperty({
+    example: "Chilonzor",
+    description: "Tuman nomi",
+  })
   @IsString()
   district: string;
 
+  @ApiProperty({
+    example: "Bunyodkor ko‘chasi",
+    description: "Ko‘cha nomi",
+  })
   @IsString()
   street: string;
 
+  @ApiPropertyOptional({
+    example: "12A",
+    description: "Uy yoki xonadon raqami",
+  })
   @IsOptional()
   @IsString()
   home?: string;
 
+  @ApiPropertyOptional({
+    example: true,
+    description: "Asosiy (main) addressmi yoki yo‘q",
+    default: false,
+  })
   @IsOptional()
   @IsBoolean()
   isMain?: boolean;
