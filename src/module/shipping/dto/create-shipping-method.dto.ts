19fff8869ca8f3dcc80f2a7129001c119efe7cce
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-14 22:58:07 +0500

    added swagger create shipping metod dto

--- src/module/shipping/dto/create-shipping-method.dto.ts
+++ src/module/shipping/dto/create-shipping-method.dto.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreateShippingMethodDto {
+  @ApiProperty({ example: "Express Shipping" })
   @IsString()
   name: string;
 
+  @ApiProperty({ example: "express" })
   @IsString()
   code: string;
 
+  @ApiProperty({ example: 8.5 })
   @IsNumber()
   price: number;
 
+  @ApiProperty({ example: 2, required: false })
   @IsOptional()
   @IsNumber()
   deliveryDays?: number;
 
+  @ApiProperty({ example: true, required: false })
   @IsOptional()
   @IsBoolean()
   isActive?: boolean;
