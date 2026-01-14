0ec3e40d805fafc302eca410201fa2c0e752ff88
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-14 23:30:00 +0500

    added swagger favorite dto

--- src/module/favorite/dto/add-favorite.dto.ts
+++ src/module/favorite/dto/add-favorite.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsInt } from "class-validator";
 
 export class AddFavoriteDto {
+  @ApiProperty({ example: 1 })
   @IsInt()
   productId: number;
 }
