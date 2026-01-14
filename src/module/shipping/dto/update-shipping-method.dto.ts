be3a1e33996f6a7524195eafe62c9482531e1642
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-14 22:58:23 +0500

    added swagger update shipping metod dto

--- src/module/shipping/dto/update-shipping-method.dto.ts
+++ src/module/shipping/dto/update-shipping-method.dto.ts
@@ -1,6 +1,9 @@
-import { PartialType } from "@nestjs/swagger";
+import { PartialType, ApiPropertyOptional } from "@nestjs/swagger";
 import { CreateShippingMethodDto } from "./create-shipping-method.dto";
 
 export class UpdateShippingMethodDto extends PartialType(
   CreateShippingMethodDto
-) {}
+) {
+  @ApiPropertyOptional({ example: false })
+  isActive?: boolean;
+}
