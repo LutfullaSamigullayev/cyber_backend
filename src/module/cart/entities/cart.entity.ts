584e38c33ceac05cb4ac95276ef6f866805b2bc8
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-14 23:40:00 +0500

    created card entity

---
+++ src/module/cart/entities/cart.entity.ts
@@ -0,0 +1,46 @@
+import { Profile } from "src/module/profile/entities/profile.entity";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { CartItem } from "./cart-item.entity";
+
+export enum CartStatus {
+  ACTIVE = "active",
+  COMPLETED = "completed",
+}
+
+@Entity("carts")
+export class Cart {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // ✅ USER EMAS, PROFILE
+  @ManyToOne(() => Profile, (profile) => profile.carts, {
+    onDelete: "CASCADE",
+  })
+  user: Profile;
+
+  @Column({
+    type: "enum",
+    enum: CartStatus,
+    default: CartStatus.ACTIVE,
+  })
+  status: CartStatus;
+
+  @OneToMany(() => CartItem, (item) => item.cart, {
+    cascade: true,
+  })
+  items: CartItem[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
