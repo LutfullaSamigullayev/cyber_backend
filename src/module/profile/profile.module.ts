af629c483df250653a5bff919af28feac00c7027
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-15 15:17:34 +0500

    update export module

--- src/module/profile/profile.module.ts
+++ src/module/profile/profile.module.ts
@@ -1,13 +1,16 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Profile } from "./entities/profile.entity";
+import { User } from "../auth/entities/user.entity";
 import { ProfileService } from "./profile.service";
 import { ProfileController } from "./profile.controller";
-import { User } from "../auth/entities/user.entity";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Profile, User])],
   controllers: [ProfileController],
   providers: [ProfileService],
+  exports: [
+    TypeOrmModule, // 🔴 SHART
+  ],
 })
 export class ProfileModule {}
