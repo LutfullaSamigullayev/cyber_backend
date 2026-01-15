788d5084e0597f94ccf87bb32b7f53072eb8f664
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-15 15:16:55 +0500

    import profile module

--- src/module/cart/cart.module.ts
+++ src/module/cart/cart.module.ts
@@ -5,10 +5,12 @@ import { CartController } from "./cart.controller";
 import { Cart } from "./entities/cart.entity";
 import { CartItem } from "./entities/cart-item.entity";
 import { Product } from "../product/entities/product.entity";
+import { ProfileModule } from "../profile/profile.module"; // ✅
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Cart, CartItem, Product]),
+    ProfileModule, // 🔴 SHART
   ],
   controllers: [CartController],
   providers: [CartService],
