36eb17e1e9aeb444020aa02c2e40f24f5c78b07f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-16 16:02:56 +0500

    updated comment module

--- src/module/comment/comment.module.ts
+++ src/module/comment/comment.module.ts
@@ -4,7 +4,6 @@ import { Comment } from "./entities/comment.entity";
 import { CommentService } from "./comment.service";
 import { CommentController } from "./comment.controller";
 import { Product } from "../product/entities/product.entity";
-import { Order } from "../order/entities/order.entity";
 import { OrderItem } from "../order/entities/order-item.entity";
 
 @Module({
@@ -12,7 +11,6 @@ import { OrderItem } from "../order/entities/order-item.entity";
     TypeOrmModule.forFeature([
       Comment,
       Product,
-      Order,
       OrderItem,
     ]),
   ],
